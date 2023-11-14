d7b097b821a1793762f4d52245f3b031711ce5d2
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-14 12:13:01 +0330

    pull

---
+++ 121-choose country and ct(0-100me)/script.js
@@ -0,0 +1,21 @@
+let objectdata={
+iran:['tehran','ghom','mashhad'],
+england:['london','liverpool','manchester'],
+usa:['a','b','c']
+}
+let cities=document.querySelector(".cities")
+let countries=document.querySelector(".countries");
+let countriesvalue=countries.value;
+
+countries.addEventListener('change',function(){
+    let countries=document.querySelector(".countries");
+let countriesvalue=countries.value;
+    // console.log(countriesvalue);
+    let citiesselect=objectdata[countriesvalue]
+    // console.log(cities);
+    cities.innerHTML=''
+    citiesselect.forEach(function(hi){
+    cities.innerHTML+='<option>'+hi+'</option>'
+    })
+    console.log(citiesselect);
+})
