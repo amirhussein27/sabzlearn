e5b27b3f8a8208e1594c97c2013d110adc70a156
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-18 14:36:51 +0330

    pull

--- 127-convert c to f/myj.js
+++ 127-convert c to f/myj.js
@@ -6,19 +6,40 @@ let change=document.getElementById("change")
 // console.log(change);
 let myinp=document.getElementById("myinp")
 
+let reset=document.getElementById("reset")
+let result=document.getElementById("result")
+let convert=document.getElementById("convert")
+
+
 change.addEventListener('click',function(){
 if(c.innerHTML==="°c"){
     c.innerHTML="°f"
     f.innerHTML="°c"
-    this.title="°c to °f"
+    document.title="convert °f to °c"
     myinp.setAttribute('placeholder','°f')
 }
 else{
     c.innerHTML="°c"
     f.innerHTML="°f"
-    this.title="°f to °c"
+    document.title="convert °c to °f"
+
     myinp.setAttribute('placeholder','°c')
+}
+})
+
+reset.addEventListener('click',function(){
+result.innerHTML="";
+myinp.value="";
+})
 
 
+convert.addEventListener('click',function(){
+    if(c.innerHTML==="°c"){
+
+  result.innerHTML=(myinp.value*9/5)+32
+}
+else{
+    console.log("object");
+    result.innerHTML=(myinp.value-32)*5/9
 }
 })
