fe04d38b7a1c0ad129d7786dddfcc49eb9752c9d
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-19 11:02:26 +0330

    pull

--- 138-KEYCODE/js.js
+++ 138-KEYCODE/js.js
@@ -2,19 +2,16 @@
 let mine=document.getElementById("mine")
 let keycode=document.getElementById("keycode")
 let location1=document.getElementById("location")
+let witch=document.getElementById("witch")
 
 document.body.addEventListener('keydown',function(event){
 // console.log("object");
+
+event.preventDefault();
 mine.innerHTML=event.key;
 keycode.innerHTML=event.keyCode;
-if(event.key==="1" || event.key==="2" || event.key==="3" || event.key==="4" || event.key==="5" || event.key==="6" || event.key==="7" || event.key==="8" || event.key==="9" || event.key==="0" || event.key==="." || event.key==="Enter" || event.key==="+" || event.key==="-" || event.key==="*" || event.key==="/" ){
-    // console.log("wdw");    
-    location1.innerHTML="Numpad"
-}
-    else{
-// console.log("object");  
-location1.innerHTML="General keys"
-
+location1.innerHTML=event.location;
+witch.innerHTML=event.which;
     }
     
-})
+)
