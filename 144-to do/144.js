2585d34ae26d21ae2b601fc7addff5123af22ff2
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-20 17:02:21 +0330

    pull

--- 144-to do/144.js
+++ 144-to do/144.js
@@ -1,7 +1,8 @@
 let inp=document.getElementsByClassName("inp")
 let btn=document.getElementById("btn")
 let new1=document.getElementById("new1")
-let trash=document.getElementsByClassName("fa fa-trash")
+let a=document.getElementsByClassName("a")
+// let trash=document.getElementsByClassName("fa fa-trash")
 let insert1=document.getElementById("insert1")
 
 function addnewtodo(newtodo){
@@ -11,6 +12,7 @@ let newtodoli=document.createElement('li')
 newtodoli.className='ab'
 let trash=document.createElement('li')
 trash.className='fa fa-trash'
+trash.id='b'
 newtodospan.innerHTML=newtodo
 newtodoli.append(newtodospan, trash)
 
@@ -18,15 +20,22 @@ insert1.append(newtodoli)
 
 // console.log(insert1);
 console.log(newtodoli);
+trash.addEventListener('click',function(event){
+    // console.log(event.target.parentElement);
+    event.target.parentElement.remove()
+})
 }
 // btn.addEventListener('click',function(){
 // new1.innerHTML+='<li id="ab"><span id="new">'+inp.value+'</span><li id="b" class="fa fa-trash"></li></li>  <br>'
 
 // })
-for(let i=0;i<trash.length;i++)
-trash[i].addEventListener('click',function(){
-    console.log("object");
-})
+// for(let i=0;i<trash.length;i++)
+// trash[i].addEventListener('click',function(event){
+//     console.log("object");
+// })
+// let trash=document.createElement('li')
+
+
 inp[0].addEventListener('keydown',function(event){
 let newtodo=event.target.value.trim();
 // console.log(newtodo);
@@ -38,3 +47,5 @@ if(event.keyCode===13){
 }
 
 })
+
+
