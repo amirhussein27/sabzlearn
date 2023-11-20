ee2654a153a927b19d59e093320af008ebac4ff7
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-20 10:11:08 +0330

    pull

---
+++ 143/143.js
@@ -0,0 +1,9 @@
+let myh1=document.getElementById("myh1")
+let btn=document.getElementById("btn")
+
+
+btn.addEventListener('click',function(){
+// myh1.remove(); The first method
+// myh1.style.display="none" The second method
+myh1.style.textDecoration="line-through"
+})
