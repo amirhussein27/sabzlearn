cf14e5865b47a1f6fda5b51dea33eeb7b688b755
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-21 13:57:30 +0330

    pull

--- 144-todo-all byme button/but.js
+++ 144-todo-all byme button/but.js
@@ -7,48 +7,52 @@ let myul=document.getElementById("myul")
 function addnewdo(newdo){
 // console.log(trash);
 
-console.log(newdo);
-let newdoli=document.createElement('li')
-newdoli.id="newli"
-let trash1=document.createElement('li')
-// console.log(newdotrash);
-let newhr=document.createElement('hr')
-
-
-trash1.className='fa fa-remove'
-trash1.id="trash"
-console.log(trash1);
-
-newdoli.innerHTML=newdo
-container.append()
-
-myul.append(newdoli,trash1,newhr)
-
-
-trash1.addEventListener('click',function(){
-    newdoli.remove()
-    trash1.remove()
-    newhr.remove()
-
-})
-deleteme.addEventListener('click',function(){
-    newdoli.remove()
-    trash1.remove()
-    newhr.remove()
-   })
-   newdoli.addEventListener('click',function(){
-    if( newdoli.style.textDecoration!="line-through"){  
-          newdoli.style.textDecoration="line-through"
-          newdoli.style.color="lightgreen"
+if(myinp.value==""){
+    alert("emptytext")
 }
-else{
-    newdoli.style.textDecoration="none"
-    newdoli.style.color="black"
-}
-
-   }
-   
-   )
+else{console.log(newdo);
+    let newdoli=document.createElement('li')
+    newdoli.id="newli"
+    let trash1=document.createElement('li')
+    // console.log(newdotrash);
+    let newhr=document.createElement('hr')
+    
+    
+    trash1.className='fa fa-remove'
+    trash1.id="trash"
+    console.log(trash1);
+    
+    newdoli.innerHTML=newdo
+    container.append()
+    
+    myul.append(newdoli,trash1,newhr)
+    
+    
+    trash1.addEventListener('click',function(){
+        newdoli.remove()
+        trash1.remove()
+        newhr.remove()
+    
+    })
+    deleteme.addEventListener('click',function(){
+        newdoli.remove()
+        trash1.remove()
+        newhr.remove()
+       })
+       newdoli.addEventListener('click',function(){
+        if( newdoli.style.textDecoration!="line-through"){  
+              newdoli.style.textDecoration="line-through"
+              newdoli.style.color="lightgreen"
+    }
+    else{
+        newdoli.style.textDecoration="none"
+        newdoli.style.color="black"
+    }
+    
+       }
+       
+       )
+    }
 
    }
 
