f19525fcf9e5e6b4855b291c3013b10f4c7a12a0
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-21 15:49:42 +0330

    pull

--- 150/source/js/script.js
+++ 150/source/js/script.js
@@ -1,1 +1,39 @@
-﻿
+﻿let contextMenu=document.getElementById("contextMenu")
+
+document.body.addEventListener('contextmenu',function(){
+// console.log(event);
+event.preventDefault()
+
+// if(contextMenu.style.display=="none")
+{
+contextMenu.style.display="block"
+    // console.log("object");
+    contextMenu.style.left=event.pageX +'px'
+    contextMenu.style.top=event.pageY +'px'
+}
+// else{
+// contextMenu.style.display="none"
+
+// }
+})
+document.body.addEventListener('click',function(){
+    // console.log(event);
+    event.preventDefault()
+    
+    if(contextMenu.style.display=="none"){
+   
+    }
+    else{
+    contextMenu.style.display="none"
+    
+    }
+    })
+// console.log(event);
+document.body.addEventListener('keydown',function(event){
+    // console.log(event.keyCode);
+if(event.keyCode==27){
+    contextMenu.style.display="none"
+
+}
+
+})
