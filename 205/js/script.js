c6ff6609e3662b2a97d69cb341ac972388564e8a
amirhussein27 <amir.eshraghi77@gmail.com>
2024-01-16 12:33:10 +0330

    hi

--- 205/js/script.js
+++ 205/js/script.js
@@ -5,17 +5,26 @@ let btn4=document.getElementById("btn4")
 let btn5=document.getElementById("btn5")
 
 btn1.addEventListener('click',function(event){
-    console.log(event.target.attributes[2]);
+    console.log(event.target.dataset.color);
+    document.documentElement.style.setProperty('--theme-color',event.target.dataset.color)
+
 })
 btn2.addEventListener('click',function(event){
-    console.log(event.target.attributes[2]);
+    console.log(event.target.dataset.color);
+    document.documentElement.style.setProperty('--theme-color',event.target.dataset.color)
+
 })
 btn3.addEventListener('click',function(event){
-    console.log(event.target.attributes[2]);
+    console.log(event.target.dataset.color);
+    document.documentElement.style.setProperty('--theme-color',event.target.dataset.color)
+
 })
 btn4.addEventListener('click',function(event){
-    console.log(event.target.attributes[2]);
+    console.log(event.target.dataset.color);
+    document.documentElement.style.setProperty('--theme-color',event.target.dataset.color)
+
 })
 btn5.addEventListener('click',function(event){
-    console.log(event.target.attributes[2]);
+    console.log(event.target.dataset.color);
+    document.documentElement.style.setProperty('--theme-color',event.target.dataset.color)
 })
