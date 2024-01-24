75ae9a48c00c1bc1ac3a894e5ca3928cfbfa30c3
amirhossein <147509867+amirhussein27@users.noreply.github.com>
2024-01-24 16:52:04 +0330

    Add files via upload
    4bahman

---
+++ ۴ بهمن/224/sabz/js/store.js
@@ -0,0 +1,8 @@
+console.log("object");
+
+let allarrays=[
+    {id:1 , title:'Album 1' , price:13.99 , img:'https://amireshraghi.ir/images/Album%201.png'},
+    {id:2 , title:'Album 1' , price:13.99 , img:'https://amireshraghi.ir/images/Album%201.png'},
+    {id:3 , title:'Album 1' , price:13.99 , img:'Images/Album 1.png'},
+    {id:4 , title:'Album 1' , price:13.99 , img:'Images/Album 1.png'}
+]
