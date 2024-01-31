3096a0de4cc3936226d337fbe31bc8e8cc735148
amirhossein <147509867+amirhussein27@users.noreply.github.com>
2024-01-31 17:10:45 +0330

    Add files via upload

---
+++ ۱۱بهمن/project my top 20 playlist - 0-100/script-me.js
@@ -0,0 +1,26 @@
+var audio = [];
+audio[0] = new Audio();
+audio[0].src = "https://www.tarafdari.com/sites/all/modules/static/external/embed.php?nid=893037";
+audio[1] = new Audio();
+// audio[1].src = "audio/cat.mp3";
+audio[2] = new Audio();
+// audio[2].src = "audio/frog.mp3";
+audio[3] = new Audio();
+// audio[3] = "audio/dog.mp3";
+
+let musicssrc=[
+  'https://amireshraghi.ir/wp-content/uploads/2023/12/SaveTube.io-Ludovico-Einaudi-Divenire-HD-320-kbps.mp3','https://ts1.tarafdari.com/contents/user196737/content-sound/metalica-the-unforgiven-2.mp3','https://dl.dibasmusic.com/dl/1401/09/Metallica-Fade-To-Black-dibamusics.com-320.mp3'
+]
+
+let myplay=document.querySelectorAll(".fa-play")
+// console.log(myplay);
+// console.log(myplay);
+let one=document.getElementById("one")
+let onei=document.getElementById("onei")
+
+// console.log(onei);
+let au1=document.getElementById("au1")
+
+let first=document.getElementById("first")
+console.log(first);
+
